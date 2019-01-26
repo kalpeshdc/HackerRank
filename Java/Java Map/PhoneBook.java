00514546b5670070ea425871495ba4a8b6a72796
Kalpesh Chaudhari <thekalpesh7@github.com>
2019-01-26 16:16:39 +0530

    Added travis ci.yml

--- Java/Java Map/PhoneBook.java
+++ Java/Java Map/PhoneBook.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * PhoneBook
@@ -5,6 +8,25 @@
 public class PhoneBook {
 
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String, Integer> phoneBook = new HashMap<>();
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+            String name = in.nextLine();
+            int phone = in.nextInt();
+            in.nextLine();
+            phoneBook.put(name, phone);
+        }
+        while (in.hasNext()) {
+            String s = in.nextLine();
+            Integer number = phoneBook.get(s);
+            if(number == null){
+                System.out.println("Not found");
+            } else{
+                System.out.println(s + "=" + number);
+            }
+        }
+        in.close();
     }
 }
--- Java/Java Map/README.md
+++ Java/Java Map/README.md
@@ -1,3 +1,5 @@
+[Java Map | HackerRank](https://www.hackerrank.com/challenges/phone-book/problem)
+
 You are given a phone book that consists of people's names and their phone number. After that you will be given some person's name as query. For each query, print the phone number of that person.
 
 **Input Format**
@@ -29,8 +31,9 @@ harry
 uncle sam
 uncle tom
 harry
-Sample Output
 ```
+
+**Sample Output**
 
 ```
 uncle sam=99912222
